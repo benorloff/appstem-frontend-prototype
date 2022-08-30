8e0759ce1853b0889fc3c1442e86e3f5022fe6f5
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-08-30 15:21:46 -0700

    set up home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,18 +2,18 @@ import react from 'react';
 
 import { Input, Menu } from 'semantic-ui-react';
 
-const navigation = () => (
-    <Menu>
-        <Menu.Item>
-
-        </Menu.Item>
-        <Menu.Item position='right'>
-            <Input
-                action={{ type: 'submit', content: 'Search'}}
-                placeholder='Search terms...'
-            />
-        </Menu.Item>
-    </Menu>
-)
-
-export default navigation;
+export default function Header() {
+    return(
+        <Menu>
+            <Menu.Item>
+                Photo Finder
+            </Menu.Item>
+            <Menu.Item position='right'>
+                <Input
+                    action={{ type: 'submit', content: 'Search'}}
+                    placeholder='Search terms...'
+                />
+            </Menu.Item>
+        </Menu>
+    )
+}
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import { Container, List, Button, Grid, Segment } from 'semantic-ui-react';
+
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import Loading from "../../components/Loader/Loader";
+import Header from "../../components/Header/Header";
+
+export default function HomePage() {
+
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    if (error) {
+        return (
+            <>
+                <Header />
+                <ErrorMessage error={error} />
+            </>
+        ) 
+    }
+
+    if (loading) {
+        return (
+            <>
+                <Header />
+                <Loading />
+            </>
+        ) 
+    }
+
+    return (
+        <>
+            <Header />
+        </>
+    )
+
+}
